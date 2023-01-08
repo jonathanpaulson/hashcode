3baff5f40500c83f7b84431d4e937f30863883c8
Jonathan Paulson <jonathanpaulson@gmail.com>
2023-01-08 11:38:18 -0500

    Simulated annealing

--- A.cc
+++ A.cc
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <random>
 using namespace std;
 using ll = int64_t;
+using ld = double;
+
+ld U(ld lo, ld hi) {
+  static default_random_engine RNG;
+  return uniform_real_distribution<ld>(lo,hi)(RNG);
+}
 
 ostream& operator<<(ostream& o, const vector<ll>& A) {
   o << "[";
@@ -11,7 +18,6 @@ ostream& operator<<(ostream& o, const vector<ll>& A) {
   o << "]";
   return o;
 }
-
 
 struct Input {
   ll B,L,D;
@@ -52,8 +58,6 @@ struct Input {
 };
 
 struct Solution {
-  vector<ll> S; // score of books; constant
-
   vector<ll> L; // permutation of 0..L-1 libraries
   vector<vector<ll>> B; // L permutations of BOOKS[i]
 
@@ -100,12 +104,37 @@ struct Solution {
   }
 };
 
+Solution simulated_annealing(const Input& I) {
+  ld t_start = 0;
+  for(ll i=0; i<I.B; i++) {
+    t_start += I.SCORE[i];
+  }
+  //temp schedule: t = t_start * (t_final / t_start) ^ time_passed, where time_passed is in 0..1
+  auto temperature = [&t_start](ld f) {
+    ld t_final = 1.0;
+    return t_start * pow(t_final / t_start, f);
+  };
+  //acceptance (when min is better): RNG() < exp((cur_result - new_result) / t), where RNG() returns 0..1 uniformly
+  auto P = [](ll s_old, ll s_new, ld t) {
+    return (s_old - s_new) / t;
+  };
+
+  Solution S = Solution::start(I);
+  ll kmax = 1000;
+  for(ll k=0; k<kmax; k++) {
+    ll t = temperature(1.0 - static_cast<ld>(k+1)/kmax);
+
+    Solution S2 = S.adjust();
+    if(U(0,1) < P(S.score(I), S2.score(I), t)) {
+      S = S2;
+    }
+  }
+  return S;
+}
+
 int main() {
   Input in = Input::read();
-  Solution S = Solution::start(in);
-  /*S.L = vector<ll>{1,0};
-  S.B[0] = vector<ll>{0,1,2,3,4};
-  S.B[1] = vector<ll>{3,1,2,0};*/
-  in.show();
+  Solution S = simulated_annealing(in);
+  //in.show();
   cout << S.score(in) << endl;
 }
